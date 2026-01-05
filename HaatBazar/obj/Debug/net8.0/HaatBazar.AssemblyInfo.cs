d63aded815a109e8a4134e4135ab3d1f00bd73a3
Shuhag Mia <shuhag.it@labaidpharma.com>
2026-01-05 10:39:31 +0600

    Updated to repository pattern

--- HaatBazar/obj/Debug/net8.0/HaatBazar.AssemblyInfo.cs
+++ HaatBazar/obj/Debug/net8.0/HaatBazar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HaatBazar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159b589564b62a7c29dccedb40526719624456dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HaatBazar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HaatBazar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
